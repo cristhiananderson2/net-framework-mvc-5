7c08d5fdf6beb0c6f5a824f873c771d37572c3b3
Cristhian Gómez <cristhian.gomez@aysconsultores.pe>
2021-12-21 13:03:09 -0500

    refactor(user): Editar y eliminar usuarios

--- ProyectoMVC5/Controllers/PersonController.cs
+++ ProyectoMVC5/Controllers/PersonController.cs
@@ -6,10 +6,58 @@ namespace ProyectoMVC5.Controllers
 {
     public class PersonController : Controller
     {
+        public static List<Person> persons =  new List<Person>();
+
+        [HttpGet]
         public ActionResult Index()
         {
             var persons = PersonDAL.List();
             return View(persons);
+        }
+        [HttpGet]
+        public ActionResult List()
+        {
+            return View(persons);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Person obj)
+        {
+            obj.Id = persons.Count + 1;
+            persons.Add(obj);
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var person = persons.Find(x => x.Id == id);
+            return View(person);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Person obj)
+        {
+            var person = persons.Find(x => x.Id == obj.Id);
+            person.Name = obj.Name;
+            person.LastName = obj.LastName;
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            persons.RemoveAll(x => x.Id == id);
+          
+            return RedirectToAction("List");
         }
 
     }
